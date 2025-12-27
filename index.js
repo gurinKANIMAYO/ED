ab8bf88e90722ba08dd84819d3fb1a4d2527a457
gurinKANIMAYO <risshi358@gmail.com>
2025-12-27 20:50:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // すべてのリクエストをkanimayo.f5.siにプロキシ
 app.use('/', createProxyMiddleware({
-  target: 'https://gointerstellar.app/',
+  target: 'https://www.croxyproxy.com/',
   changeOrigin: true,
   ws: true,
   secure: false // 証明書が自己署名などの場合に必要
